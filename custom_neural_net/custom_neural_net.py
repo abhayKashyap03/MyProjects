9635c4c3b8a8c3408812a561525fc822504d5ff3
Abhay Kashyap <abhay.kashyap03@gmail.com>
2020-12-01 18:17:20 +0530

    updated project

--- custom_neural_net/custom_neural_net.py
+++ custom_neural_net/custom_neural_net.py
@@ -1,18 +1,18 @@
-# Custom Neural Network - building neural network from scratch
-
 import numpy as np
-
 
 class NeuralNet:
     def __init__(self, x, y, weights1, bias1, weights2, bias2, weights3, bias3):
         # Inputs and Target Outputs
-        self.inp = x
+        self.x = x
         self.y = y
+        self.training_size = self.x.shape[0]
 
         self.n = 100  # number of data points
         self.c = 3  # number of classes
         self.out = np.zeros(self.y.shape)  # predicted output
-        self.lr = 0.05  # learning rate for model
+        self.lr = 1  # learning rate for model
+        self.reg_strength = 0.05
+        self.activation = self.softmax
 
         # Weights and Bias for Hidden Layers
         self.weights1 = weights1
@@ -22,55 +22,50 @@ class NeuralNet:
         self.weights3 = weights3
         self.bias3 = bias3
 
-    # Activation function used is sigmoid, other functions like ReLU, Leaky ReLU, Tanh can also be used
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
-    def sigmoid_deriv(self, x):
-        return self.sigmoid(x) * (1 - self.sigmoid(x))
+    def ReLU(self, x):
+        return np.maximum(0, x)
 
     def softmax(self, x):
         exp = np.exp(x)
-        return exp/np.sum(exp, axis=1, keepdims=True)
+        return exp / np.sum(exp, axis=1, keepdims=True)
 
     def feedforward(self):
-        self.layer1 = self.sigmoid(np.dot(self.inp, self.weights1) + self.bias1)
-        self.layer2 = self.sigmoid(np.dot(self.layer1, self.weights2) + self.bias2)
+        self.layer1 = self.activation(np.dot(self.x, self.weights1) + self.bias1)
+        self.layer2 = self.activation(np.dot(self.layer1, self.weights2) + self.bias2)
         self.out = np.dot(self.layer2, self.weights3) + self.bias3
-        print(self.out)
-
-    def mse(self):
-        diff = self.out - self.y
-        diff_sq = diff ** 2
-        m_s_e = diff_sq.mean()
-        return m_s_e
-
-    def crossentropy_l2(self):
-        correct_x = (self.y * np.log(self.softmax(self.out))) + (1-self.y) * (np.log(1 - self.softmax(self.out)))
-        loss = -np.sum(correct_x)
-        return loss
-
 
     def backprop(self):
-        error = self.mse()
-        print("self.out : ", self.out.shape, "\nself.layer2 : ", self.layer2.shape, "\nself.layer1 : ", self.layer1.shape, "\nself.weights3 : ", self.weights3.shape, "\nself.weights2 : ",
-        self.weights2.shape, "\nself.weights1 : ", self.weights1.shape)
+        self.prob = self.softmax(self.out)
+        d_out = self.prob.copy()
+        d_out[range(self.training_size), self.y] -= 1
+        d_out /= self.training_size
 
-        scores = self.out
-        prob = self.softmax(scores)
+        dW3 = np.dot(self.layer2.T, d_out)
+        dB3 = np.sum(d_out, axis=0, keepdims=True)
+        dhidden2 = np.dot(d_out, self.weights3.T)
+        dhidden2[self.layer2 <= 0] = 0
 
+        dW2 = np.dot(self.layer1.T, dhidden2)
+        dB2 = np.sum(dhidden2, axis=0, keepdims=True)
+        dhidden1 = np.dot(dhidden2, self.weights2.T)
+        dhidden1[self.layer1 <= 0] = 0
 
+        dW1 = np.dot(self.x.T, dhidden1)
+        dB1 = np.sum(dhidden1, axis=0, keepdims=True)
 
-        d_weights3 = np.dot(self.layer2.T, np.dot(error, self.sigmoid_deriv(self.out)))
-        d_bias3 = np.sum(np.dot(error, self.sigmoid_deriv(self.out)), axis=0, keepdims=True)
-        d_weights2 = np.dot(self.layer1.T, np.dot(np.dot(error, self.sigmoid_deriv(self.out)), self.weights3.T))
-        d_bias2 = np.sum(np.dot(np.dot(error, self.sigmoid_deriv(self.out)), self.weights3.T), axis=0, keepdims=True)
-        d_weights1 = np.dot(self.inp.T, np.dot(np.dot(error, self.sigmoid_deriv(self.out)), self.weights2.T))
-        d_bias1 = np.sum(np.dot(np.dot(error, self.sigmoid_deriv(self.out)), self.weights2.T), axis=0, keepdims=True)
+        self.weights3 -= self.lr * dW3
+        self.weights2 -= self.lr * dW2
+        self.weights1 -= self.lr * dW1
+        self.bias3 -= self.lr * dB3
+        self.bias2 -= self.lr * dB2
+        self.bias1 -= self.lr * dB1
 
-        self.weights3 -= self.lr * d_weights3
-        self.bias3 -= self.lr * d_bias3
-        self.weights2 -= self.lr * d_weights2
-        self.bias2 -= self.lr * d_bias2
-        self.weights1 -= self.lr * d_weights1
-        self.bias1 -= self.lr * d_bias1
+    def loss(self):
+        correct_prob = self.y * np.log(self.prob[range(self.training_size), self.y])
+        loss = -np.sum(correct_prob) / self.training_size
+        regularization = 0.5 * self.reg_strength * ((np.sum(self.weights1 * self.weights1) + np.sum(self.weights2 * self.weights2) + np.sum(self.weights3 * self.weights3)) / self.training_size)
+        total_loss = loss + regularization
+        return total_loss
--- custom_neural_net/main.py
+++ custom_neural_net/main.py
@@ -1,34 +1,39 @@
-# Main file - run custom neural network from this file
-
 import numpy as np
-from custom_neural_net import *
+from custom_neural_net import NeuralNet
 
 n = 100  # number of data points
 c = 3  # number of classes
 d = 2  # dimensions
-epochs = 1000  # number of epochs/iterations
 x = np.zeros((n*c, d))  # input data
 y = np.zeros((n*c), dtype='uint8')  # target outputs
+epochs = 25000  # number of epochs/iterations
+training_size = x.shape[0]
+lr = 10
+reg_strength = 0.005
 
 for i in range(c):
     xi = range(n*i, n*(i+1))
-    r = np.linspace(0, 1, n)
-    t = np.linspace(i*4, (i+1)*4, n) + np.random.random(n)*0.2
+    r = np.linspace(0, 1, n)  # radius
+    t = np.linspace(i*4, (i+1)*4, n) + np.random.random(n)*0.2  # theta
     x[xi] = np.c_[r*np.sin(t), r*np.cos(t)]
     y[xi] = i
-y = np.eye(c)[y]
-print(y.shape)
+
 # Random Weights and Bias for Hidden Layers
-weights1 = np.random.random((x.shape[1], n))
+weights1 = np.random.randn(d, n) * 0.01
 bias1 = np.zeros((1, n))
-weights2 = np.random.random((n, n))
+weights2 = np.random.randn(n, n) * 0.01
 bias2 = np.zeros((1, n))
-weights3 = np.random.random((n, c))
+weights3 = np.random.randn(n, c) * 0.01
 bias3 = np.zeros((1, c))
 
 net = NeuralNet(x, y, weights1, bias1, weights2, bias2, weights3, bias3)
 
 for i in range(epochs):
     net.feedforward()
-    print("[Epoch %d] Loss : %f" % ((i+1), net.crossentropy_l2()))
     net.backprop()
+    if i % 1000 == 0:
+        print("[Epoch %d] Loss : %f" % (i, net.loss()))
+
+net.feedforward()
+pred = np.argmax(net.out, axis=1)
+print("Accuracy : %f" % (np.mean(pred == y)))
